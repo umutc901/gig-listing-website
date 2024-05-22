566b3f8b9f92e4e09443fa3f0668c6464a9ada3b
Umut <umutc901@gmail.com>
2024-05-22 11:49:48 +0100

    Gig component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import Hello from "./components/Hello";
 import makersLogo from "./assets/Makers-Logo.png";
+import Gig from "./components/Gig";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Hello name="World" />
-      <img className="logo" src={makersLogo}></img>
+      <Hello name="Finder" />
+      <Gig></Gig>
     </>
   );
 }
+
 
 export default App;
---
+++ src/components/Gig.jsx
@@ -0,0 +1,12 @@
+const Gig = () => {
+    return(
+        <div id ="gig">
+            <h3 id="band-name">Pink Floyd</h3>
+            <img id="band-image" src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d6/Pink_Floyd_-_all_members.jpg/250px-Pink_Floyd_-_all_members.jpg"></img>
+            <p id="event-description">The Darkside of the moon Tour</p>
+            <p id="time-date">20:00, 17/01/1972</p>
+            <p id="event-location">Rainbow Theatre, London</p>
+        </div>
+    )
+}
+export default Gig;
