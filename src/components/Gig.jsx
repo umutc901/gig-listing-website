2bd3d93b792259315f727c9b89b4950d61952b88
Umut <umutc901@gmail.com>
2024-05-23 14:52:34 +0100

    added favourite button functionality

--- src/components/Gig.jsx
+++ src/components/Gig.jsx
@@ -1,14 +1,28 @@
+import React, { useState } from 'react';
 import "../Gig.css";
 
 const Gig = (props) => {
-    return(
+    const [isFavorited, setIsFavorited] = useState(false);
+
+    const toggleFavorite = () => {
+        setIsFavorited(prevState => !prevState);
+    };
+
+    return (
         <div className="gig">
-        <h3 className="band-name">{props.bandName}</h3>
-        <img className="band-image" src={props.src} alt="Band" />
-        <p className="event-description">{props.eventDescription}</p>
-        <p className="time-date">{props.timeDate}</p>
-        <p className="event-location">{props.eventLocation}</p>
-      </div>
-    )
-}
+            <h3 className="band-name">{props.bandName}</h3>
+            <img className="band-image" src={props.src} alt="Band" />
+            <p className="event-description">{props.eventDescription}</p>
+            <p className="time-date">{props.timeDate}</p>
+            <p className="event-location">{props.eventLocation}</p>
+            <button
+                className={`favorite-button ${isFavorited ? 'favorited' : ''}`}
+                onClick={toggleFavorite}
+            >
+                <i className={`fa-heart ${isFavorited ? 'fas' : 'far'}`}></i>
+            </button>
+        </div>
+    );
+};
+
 export default Gig;
